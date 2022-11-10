23be54f6ebc1432fa71a2c6361b52f7afdb17ede
Dominik <gamerdomi21@googlemail.com>
2022-11-10 21:18:09 +0100

    installed tailwind; deleted default styles, completed first new composition api test.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1 class="text-6xl mb-6 text-center font-bold">Pokedex</h1>
+    <h3 class="text-4xl mb-6 text-center">Wir bauen einen Pokedex mit Vue und Vite</h3>
+    <p class="text-center font-bold">Und nutzen dafür die Poke API </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <input type="text" v-model="filterText"/>
+  <div class="poke-data">
+    <!-- Old options api Looping over data  -->
+    <!-- <li v-for="(pokemon, index) in pokemonList" :key="`poke-${ index }`">
+      {{ pokemon.pokemon_species }}
+    </li> -->
+    <li class="text-center" v-for="(pokemon, index) in pokemonList" :key="`poke-${ index }`">
+      #{{pokemon.entry_number}} - {{pokemon.pokemon_species.name}}
+    </li>
+  </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const pokemonList = ref([])
+
+onMounted(async () => {
+  const pokemonData = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
+    .then(response => response.json()
+    )
+  pokemonList.value = pokemonData.pokemon_entries
+})
+
+
+
+//*** 
+//OLD options api 
+// export default {
+//   data: () => ({
+//     pokemonList: []
+//   }), 
+//   async mounted() {
+//     const pokemonData = await fetch('https://pokeapi.co/api/v2/pokedex/2/').then(
+//       response => response.json()
+//     )
+//     this.pokemonList = pokemonData.pokemon_entries
+//     console.log({pokemonData})
+//   },
+// }
+</script>
+
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.poke-data{
+  margin-top: 2rem;
 }
 </style>
