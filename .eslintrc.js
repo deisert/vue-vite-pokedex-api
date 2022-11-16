e1f6f41d916e52ef638af25906c5080f569c557d
Dominik <gamerdomi21@googlemail.com>
2022-11-16 19:42:45 +0100

    added rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
+  env: {
+    node: true,
+  },
+
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
+    "eslint:recommended",
     "plugin:vue/vue3-recommended",
+    "prettier",
     // 'plugin:vue/vue3-essential', // This option doesn't impose formatting rules
     // 'plugin:vue/vue3-strongly-recommended', // This option imposes formatting rules on your code to improve readability
   ],
   rules: {
     // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    "vue/no-unused-vars": "error",
   },
 };
