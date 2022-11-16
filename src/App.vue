66342a9fed90afa94e59134b65fb349049da0049
Dominik <gamerdomi21@googlemail.com>
2022-11-16 19:43:03 +0100

    added prettier and formatted

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,49 @@
 <template>
   <div class="container-wrapper">
-  <div class="headlines">
-    <h1 class="text-6xl mb-6 text-center font-bold">Pokedex</h1>
-    <h4 class="text-3xl mb-6 text-center">Wir bauen einen Pokedex mit Vue und Vite</h4>
-    <p class="text-center font-bold">Und nutzen dafür die Poke API </p>
-  </div>
-  <div class="pokemon-data">
-      <input type="text" class="block m-auto border border-gray-900 rounded-md p-2 focus:outline focus:#ff1f1f focus:#ff1f1f focus:ring-1 my-6"
-        name="pokemon-search"  
-        placeholder="search for pokemon" 
+    <div class="headlines">
+      <h1 class="text-6xl mb-6 text-center font-bold">Pokedex</h1>
+      <h4 class="text-3xl mb-6 text-center">
+        Wir bauen einen Pokedex mit Vue und Vite
+      </h4>
+      <p class="text-center font-bold">Und nutzen dafür die Poke API</p>
+    </div>
+    <div class="pokemon-data">
+      <input
+        type="text"
+        class="block m-auto border border-gray-900 rounded-md p-2 focus:outline focus:#ff1f1f focus:#ff1f1f focus:ring-1 my-6"
+        name="pokemon-search"
+        placeholder="search for pokemon"
         v-model="filterText"
       />
-    <li class="text-center" v-for="(pokemon, index) in pokemonList" :key="`poke-${ index }`">
-      #{{pokemon.entry_number}} - {{pokemon.pokemon_species.name}}
-    </li>
-  </div>
+      <li
+        class="text-center"
+        v-for="(pokemon, index) in pokemonList"
+        :key="`poke-${index}`"
+      >
+        #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}
+      </li>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted } from "vue";
 
-const pokemonList = ref([])
+const pokemonList = ref([]);
 
 onMounted(async () => {
-  const pokemonData = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
-    .then(response => response.json()
-    )
-  pokemonList.value = pokemonData.pokemon_entries
-})
+  const pokemonData = await fetch("https://pokeapi.co/api/v2/pokedex/2/").then(
+    (response) => response.json()
+  );
+  pokemonList.value = pokemonData.pokemon_entries;
+});
 
-
-
-//*** 
-//OLD options api 
+//***
+//OLD options api
 // export default {
 //   data: () => ({
 //     pokemonList: []
-//   }), 
+//   }),
 //   async mounted() {
 //     const pokemonData = await fetch('https://pokeapi.co/api/v2/pokedex/2/').then(
 //       response => response.json()
@@ -48,10 +54,8 @@ onMounted(async () => {
 // }
 </script>
 
-
-
 <style scoped>
-.poke-data{
+.poke-data {
   margin-top: 2rem;
 }
 </style>
