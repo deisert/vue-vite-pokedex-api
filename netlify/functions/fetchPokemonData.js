eb6c6ca5c18b1b50d4d43a9adc95c371b209301a
Dominik <gamerdomi21@googlemail.com>
2022-11-19 17:07:23 +0100

    added comments

--- netlify/functions/fetchPokemonData.js
+++ netlify/functions/fetchPokemonData.js
@@ -1,6 +1,9 @@
 //package for easier fetch
-//const fetch = require("node-fetch");
+//I dont use webpack in this project, so the import looks like this
 import fetch from "node-fetch";
+
+//and not like this
+//const fetch = require("node-fetch");
 
 exports.handler = async function () {
   const pokemonData = await fetch("https://pokeapi.co/api/v2/pokedex/2/").then(
