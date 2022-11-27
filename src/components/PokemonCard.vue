c811ec43cfceba7de6feba6cef888f9ed47e2973
Dominik <gamerdomi21@googlemail.com>
2022-11-27 18:54:17 +0100

    Created PokemonCard Component

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pokemon-card">hallo</div>
+</template>
+
+<script setup>
+import { reactive, ref, defineComponent } from "vue";
+</script>
+
+<style scoped>
+.pokemon-card {
+  border: 1px solid black;
+}
+</style>
