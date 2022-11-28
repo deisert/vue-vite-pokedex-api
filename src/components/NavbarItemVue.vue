a66e42730f26457e3a31f19bc613b814ba9ba36d
Dominik <gamerdomi21@googlemail.com>
2022-11-28 20:13:30 +0100

    removed unnecessary import and fiddled with props declaration

--- src/components/NavbarItemVue.vue
+++ src/components/NavbarItemVue.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="nav-item w-16 h-16 rounded-lg border border-black">
+  <div
+    class="nav-item px-2 min-w-max h-14 text-center rounded-lg border border-black cursor-pointer"
+    @click="doSomething()"
+  >
     {{ description }}
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+//import { defineProps } from "vue";
 
 const props = defineProps({
   description: String,
 });
+
+function doSomething() {
+  console.log(props.description);
+}
 </script>
 
 <style scoped></style>
