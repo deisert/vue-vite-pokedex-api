ad994703437eacc1da72ba0997507e3f1f0a6f07
Dominik <gamerdomi21@googlemail.com>
2022-12-03 19:13:22 +0100

    Created first version of cards.

--- src/components/BerryCardVue.vue
+++ src/components/BerryCardVue.vue
@@ -1,7 +1,39 @@
 <template>
-  <div class="berrie-card"></div>
+  <div class="berry-card">
+    <div
+      class="max-w-sm border bg-slate-50 border-red-300 rounded-lg shadow-md"
+    >
+      <a href="#">
+        <img
+          class="rounded-t-lg w-full h-48"
+          src="src\assets\Berry_Basket.png"
+          alt="berry basket"
+        />
+      </a>
+      <div class="p-3">
+        <h5 class="berry-name mb-1 text-2xl font-semibold">Berry Name</h5>
+
+        <p class="berry-description mb-2 font-thin">I am berry text</p>
+      </div>
+    </div>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { reactive, ref, onMounted } from "vue";
+
+//Object to store berries inside
+const berrieStore = reactive({
+  list: [],
+});
+
+onMounted(async () => {
+  const berrieData = await fetch("/.netlify/functions/fetchBerrieData").then(
+    (response) => response.json()
+  );
+  berrieStore.list = berrieData;
+  //console.log({ berrieStore });
+});
+</script>
 
 <style scoped>
 .berrie-card {
