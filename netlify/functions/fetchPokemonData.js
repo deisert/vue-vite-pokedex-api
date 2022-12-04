38f4ab31d9eacc728c3d9247f09dc1f3737026cc
Dominik <gamerdomi21@googlemail.com>
2022-12-04 23:54:32 +0100

    added force cache to test things.

--- netlify/functions/fetchPokemonData.js
+++ netlify/functions/fetchPokemonData.js
@@ -6,9 +6,9 @@ import fetch from "node-fetch";
 //const fetch = require("node-fetch");
 
 exports.handler = async function () {
-  const pokemonDataApi = await fetch(
-    "https://pokeapi.co/api/v2/pokedex/2/"
-  ).then((response) => response.json());
+  const pokemonDataApi = await fetch("https://pokeapi.co/api/v2/pokedex/2", {
+    cache: "force-cache",
+  }).then((response) => response.json());
 
   //"mapping" the api response json to access it easier in the frontend
   const pokemonEntries = pokemonDataApi.pokemon_entries
